d6d304455eaea76b0cb0fa2de29d4019d94a9655
Tyson Hall <tn8938@gmail.com>
2023-10-13 16:02:12 -0400

    Added Donut class

---
+++ donut-shop/src/main/java/com/pluralsight/Donut.java
@@ -0,0 +1,32 @@
+package com.pluralsight;
+
+public class Donut {
+    String type;
+    float price;
+    float calories;
+    String description;
+
+
+    Donut() {
+        type = "Glazed";
+        price = 2;
+        calories = 300;
+        description = "Shiny";
+    }
+    Donut (String type, float calories, float price, String description) {
+        this.type = type;
+        this.calories = calories;
+        this.price = price;
+        this.description = description;
+
+    }
+
+    public String toString() {
+        return "Donut {" +
+                "type = " + type +
+                ", calories = '" + calories + '\'' +
+                ", price = '" + price + '\'' +
+                ", description = '" + description + '\'' +
+                '}' + "\n";
+    }
+}
---
+++ donut-shop/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,64 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Donut d = new Donut();
+        System.out.println(d);
+
+        Donut d1 = new Donut("cake", 300, 1, "cakey");
+        Donut d2 = new Donut("vanilla", 400, 3, "white");
+        Donut d3 = new Donut("chocolate", 500, 2, "dark brown");
+        Donut d4 = new Donut("french curl", 600, 5, "curly");
+        Donut d5 = new Donut("strawberry", 500, 9, "red");
+        Donut d6 = new Donut("mango", 300, 7, "yellow");
+
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+        System.out.println(d4);
+        System.out.println(d5);
+        System.out.println(d6);
+
+        Donut myDonuts[] = new Donut[6];
+        myDonuts[0] = d1;
+        myDonuts[1] = d2;
+        myDonuts[2] = d3;
+        myDonuts[3] = d4;
+        myDonuts[4] = d5;
+        myDonuts[5] = d6;
+
+        for (Donut donut : myDonuts) {
+            System.out.println(donut + "\n");
+        }
+
+        // totalPrice(myDonuts);
+    }
+
+    /*
+    public static float totalPrice(Donut[] d) {
+        for (Donut d)
+        return;
+    }
+     */
+}
+
+/** Project prompt
+ * Project: donut-shop
+ *
+ * class:  Donut
+ *
+ * type, calories, price, description
+ *
+ * Generate constructor
+ * Generate a toString method
+ *
+ * class: Main
+ *
+ * Make (use the new operator) at least 6 donuts and print them out
+ * Save them in an array
+ * Write a loop that goes through the array and prints out the donuts
+ *
+ * Extra credit:  print out the total price for the donuts
+ * */
